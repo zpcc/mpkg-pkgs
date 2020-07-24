e2001d54d8a4a2c9c7d4d0dd4063578ea4c37a2a
zp <zp.c@outlook.com>
2020-07-24 13:30:16 +0800

    add a parser and git

---
+++ packages/git.py
@@ -0,0 +1,21 @@
+from mpkg.common import Soft
+from mpkg.load import Load
+
+
+class Package(Soft):
+    ID = 'git'
+    SilentArgs = '/sp- /verysilent /norestart'
+
+    def _prepare(self):
+        parser = Load(
+            'https://github.com/zpcc/mpkg-pkgs/raw/master/parser/common.py')[0][0].github
+        url = 'https://github.com/git-for-windows/git/releases/latest'
+        header, links, self.date = parser(url)
+        self.log = url
+        self.ver = header.split(' ')[-1]
+        self.link = {}
+        for link in links:
+            if link.endswith('-64-bit.exe'):
+                self.link['64bit'] = link
+            elif link.endswith('-32-bit.exe'):
+                self.link['32bit'] = link
--- parser/common.py
+++ parser/common.py
@@ -25,3 +25,16 @@ class Package(Soft):
             result = [(item.xpath('.//span')[0].text,
                        item.xpath('.//abbr')[0].text) for item in (folers+files)]
             return result
+
+    @staticmethod
+    def github(url: str):
+        page = etree.HTML(GetPage(url))
+        #release=page.xpath('//div[contains(@class, "release-main-section")]')[0]
+        header = page.xpath('//*[@class="release-header"]')[0]
+        text = header.xpath('./div/div/a')[0].text
+        date = header.xpath('.//relative-time')[0].values()[0][:10]
+        assests = page.xpath(
+            '//*[@class="Box Box--condensed mt-3"]/div')[0].xpath('.//div')
+        links = ['https://github.com' +
+                 item.xpath('./a')[0].values()[0] for item in assests]
+        return text, links, date
