682918bac14f9a79b01cf3efabcf2004bc8ef112
zp <zp.c@outlook.com>
2020-07-26 13:33:22 +0800

    fix: wrong date

--- packages/Driver.Intel.Wireless.Admin.py
+++ packages/Driver.Intel.Wireless.Admin.py
@@ -7,11 +7,14 @@ from mpkg.utils import GetPage
 
 
 def getIntelDrivers(u) -> list:
-    r = GetPage(u)
+    page = etree.HTML(GetPage(u))
     u = [x.xpath('.//a')[0].values()[1]
-         for x in etree.HTML(r).xpath('//*[@class="download-file"]')]
+         for x in page.xpath('//*[@class="download-file"]')]
     drivers = [unquote(x).split('httpDown=')[::-1][0] for x in u]
-    return drivers
+    version = page.xpath('//*[@class="version"]/span[2]')[0].text.strip()
+    date = page.xpath('//*[@class="date"]/span[2]')[0].text.strip()
+    date = time.strftime('%Y-%m-%d', time.strptime(date, '%m/%d/%Y'))
+    return drivers, version, date
 
 
 class Package(Soft):
@@ -20,17 +23,12 @@ class Package(Soft):
     def _prepare(self):
         page = GetPage(
             'https://www.intel.com/content/www/us/en/support/articles/000017246/network-and-i-o/wireless-networking.html')
-        tmp = [x for x in etree.HTML(page).xpath('//p')
-               if b'Last Reviewed' in etree.tostring(x)][0]
-        date = list(tmp.itertext())[2].replace(' ', '').strip()
-        self.date = time.strftime('%Y-%m-%d', time.strptime(date, '%m/%d/%Y'))
         tmp = [x for x in etree.HTML(page).xpath('//a')
                if b'Download Here' in etree.tostring(x)]
         if len(tmp) == 1:
-            self.links = sorted(getIntelDrivers(
-                tmp[0].values()[0]), reverse=True)
-            for link in self.links:
-                if link.endswith('_all.zip'):
-                    self.ver = link.split('_all.zip')[0].split('_')[-1]
+            drivers, version, date = getIntelDrivers(tmp[0].values()[0])
+            self.links = sorted(drivers, reverse=True)
+            self.date = date
+            self.ver = version
         else:
             print('IntelWifi(Soft) parsing error')
