dd87ca6d62e80a138651e26a8b3980032895840d
zp <zp.c@outlook.com>
2020-08-11 20:54:57 +0800

    fix vlc.py

--- packages/vlc.py
+++ packages/vlc.py
@@ -7,9 +7,9 @@ class Package(Soft):
 
     def _prepare(self):
         data = self.data
-        arch = {'32bit': 'https://get.videolan.org/vlc/last/win32/vlc-{ver}-win32.exe',
-                '64bit': 'https://get.videolan.org/vlc/last/win64/vlc-{ver}-win64.exe'}
-        url = 'https://get.videolan.org/vlc/last/win64/'
+        arch = {'32bit': 'https://download.videolan.org/pub/videolan/vlc/last/win32/vlc-{ver}-win32.exe',
+                '64bit': 'https://download.videolan.org/pub/videolan/vlc/last/win64/vlc-{ver}-win64.exe'}
+        url = 'https://download.videolan.org/pub/videolan/vlc/last/win64/'
         data.ver = Search(url, 'vlc-([\\d.]+)-win64.exe')
         data.arch = Search(links=arch, ver=data.ver)
         for k, v in data.arch.items():
