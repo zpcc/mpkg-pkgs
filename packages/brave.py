67be0abce897aee786aa10841ccd5a29c4075e05
zp <zp.c@outlook.com>
2020-08-28 19:24:56 +0800

    fix brave.py

--- packages/brave.py
+++ packages/brave.py
@@ -10,9 +10,9 @@ class Package(Soft):
         data = self.data
         parser = Load('http/common-zpcc.py', sync=False)[0][0].github
         url = 'https://github.com/brave/brave-browser/raw/master/CHANGELOG_DESKTOP.md'
-        ver = Search(url, r'(?m)^## \[([\d.]*)\]')
+        data.ver = Search(url, r'(?m)^## \[([\d.]*)\]')
         _, links, data.date = parser(
-            f'https://github.com/brave/brave-browser/releases/tag/v{ver}')
+            f'https://github.com/brave/brave-browser/releases/tag/v{data.ver}')
         for link in links:
             if link.endswith('BraveBrowserStandaloneSilentSetup.exe'):
                 data.arch['64bit'] = link
