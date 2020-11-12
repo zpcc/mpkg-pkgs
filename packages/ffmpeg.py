dca47baf828b546814bd73ab6fdb7cd6200f69ed
zp <zp.c@outlook.com>
2020-11-12 11:17:07 +0800

    fix ffmpeg

--- packages/ffmpeg.py
+++ packages/ffmpeg.py
@@ -1,5 +1,6 @@
 from mpkg.common import Soft
 from mpkg.load import Load
+from mpkg.utils import GetPage
 
 
 class Package(Soft):
@@ -9,10 +10,12 @@ class Package(Soft):
         data = self.data
         data.bin = [r'bin\ffmpeg.exe', r'bin\ffplay.exe', r'bin\ffprobe.exe']
         parser = Load('http/common-zpcc.py', sync=False)[0][0].github
-        url = 'https://github.com/GyanD/codexffmpeg/releases/latest'
+        url = 'https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-github'
         header, links, data.date = parser(url)
         data.ver = header.split(' ')[1]
         data.changelog = 'https://ffmpeg.org/index.html#news'
         # 'ffmpeg-([\\d.-]+)-full_build-shared.(zip|7z)</a>'
         link = [link for link in links if 'full_build-shared.7z' in link][0]
         data.arch = {'64bit': link}
+        data.sha256 = {'64bit': GetPage(
+            'https://www.gyan.dev/ffmpeg/builds/sha256-release-full-shared')}
