9dedd84eb0e6cebc0dff85284f9116b67c2b09fc
zp <zp.c@outlook.com>
2021-04-07 19:05:30 +0800

    fix wget

--- packages/wget.py
+++ packages/wget.py
@@ -10,8 +10,8 @@ class Package(Soft):
         data.bin = ['wget.exe']
         links = {'32bit': 'https://eternallybored.org/misc/wget/releases/wget-{ver}-win32.zip',
                  '64bit': 'https://eternallybored.org/misc/wget/releases/wget-{ver}-win64.zip'}
-        url = 'https://eternallybored.org/misc/wget'
-        regex = '<title>GNU Wget ([\\d.]+) for Windows'
+        url = 'https://eternallybored.org/misc/wget/releases/?C=M;O=D'
+        regex = '>wget-([\\d.-]+)-win64\\.zip</a>'
         data.changelog = 'https://eternallybored.org/misc/wget'
         data.ver = Search(url, regex)
         data.arch = Search(links=links, ver=data.ver)
